b252c476e434afcd9796250f025822ffac6a38e2
NahyeonAn <anna@kitech.re.kr>
2022-08-04 17:45:57 +0900

    a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'anna'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser',
+extensions = [
               'sphinx.ext.autodoc',
               'sphinx.ext.mathjax'
 ]
